193b199c2ff69a0853f165b2234a76d515a1f670
Emilio Moretti <emilio.moretti@gmail.com>
2021-03-07 19:13:14 +0100

    Disable workarounds by default

--- build.sh
+++ build.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
+echo if the compilation fails with error 8, it\'s a bug in wasm-pack
+echo https://github.com/rustwasm/wasm-pack/issues/974
+echo open Cargo.toml and disable the optimization step
+echo and run it manually like:
+echo wasm-opt pkg/req_anim_frame.wasm -o ../demo_server/web_app/req_anim_frame.wasm_bg.wasm -O4 -all -ffm --enable-simd                                                         
+
 rm demo_server/web_app/req_anim_frame.wasm_bg.wasm demo_server/web_app/req_anim_frame.js
 cd req_anim_frame
 wasm-pack build --target web --out-name req_anim_frame.wasm || exit 1
 cp pkg/req_anim_frame.js ../demo_server/web_app/
-
-if command -v wasm-opt > /dev/null 2>&1; then
-  wasm-opt pkg/req_anim_frame.wasm -o ../demo_server/web_app/req_anim_frame.wasm_bg.wasm -O4 -all -ffm --enable-simd
-else
-  cp pkg/req_anim_frame.wasm ../demo_server/web_app/req_anim_frame.wasm_bg.wasm
-  echo WARNING: wasm-opt is not available. You can install it globally, or
-  echo enable it in Cargo.toml if you are running on an x64 linux system.
-  echo Cargo auto-installation for this particular command is broken
-  echo because there are no precompiled packages.
-fi
-
+cp pkg/req_anim_frame.wasm ../demo_server/web_app/req_anim_frame.wasm_bg.wasm
 cd -
-echo COMPILATION DONE. OPENING DEV SERVER:
+echo
+echo %%%%%%%% COMPILATION DONE. OPENING DEV SERVER: %%%%%%%
 python3 demo_server/server.py
