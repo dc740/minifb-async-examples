eec1c1ecc0fadd51e5103c3796b85b785bb2bede
Emilio Moretti <emilio.moretti@gmail.com>
2021-04-26 00:33:11 +0200

    Allow demo server to server SharedArrayBuffer wasm
    The new implementation for SharedArrayBuffer requires
    to be in a secure context. These headers must be present
    but it also requires you to either sever it through localhost
    or serve it through HTTPS. To test if you are in a secure
    context just check window.crossOriginIsolated in javascript.
    
    I think the demo server should be in a gist, or its own
    repo, since it's getting more and more useful for
    testing purposes. Just like my web services simulator.

--- demo_server/server.py
+++ demo_server/server.py
@@ -15,13 +15,28 @@ class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         '.html': 'text/html',
         '.png': 'image/png',
         '.jpg': 'image/jpg',
-        '.svg':	'image/svg+xml',
-        '.css':	'text/css',
-        '.js':'application/x-javascript',
+        '.svg': 'image/svg+xml',
+        '.css': 'text/css',
+        '.js': 'application/x-javascript',
         '.wasm': 'application/wasm',
         '.json': 'application/json',
         '.xml': 'application/xml',
     }
+
+    def end_headers(self):
+        """
+        Overrides the SimpleHTTPRequestHandler to
+        send extra headers.
+        """
+        self.send_safe_context_headers()
+        super().end_headers()
+
+    def send_safe_context_headers(self):
+        """
+        Allows SharedArrayBuffer to work from our demo server.
+        """
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
 
 with socketserver.TCPServer(("localhost", PORT), HttpRequestHandler) as httpd:
     try:
